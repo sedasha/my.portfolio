7a8b95577d334ee895cb02e304cec8c7d66cd704
sedasha <seda_sha@yahoo.com>
2021-06-27 17:31:50 -0400

    Updates

---
+++ precache-manifest.e09f2ef2f252de56f469a6754f779f18.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f0e121e0ded97462fa73422635b35dc",
+    "url": "/my.portfolio/index.html"
+  },
+  {
+    "revision": "18ce35aef43e7cdacb1f",
+    "url": "/my.portfolio/static/css/main.b5cc8953.chunk.css"
+  },
+  {
+    "revision": "b1535ff8cd8c0e542f96",
+    "url": "/my.portfolio/static/js/2.325da840.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/my.portfolio/static/js/2.325da840.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18ce35aef43e7cdacb1f",
+    "url": "/my.portfolio/static/js/main.9f1e67d2.chunk.js"
+  },
+  {
+    "revision": "5c7363fdafb4bd8cb065",
+    "url": "/my.portfolio/static/js/runtime-main.36c23747.js"
+  },
+  {
+    "revision": "dd6e1bdea017d7a27ab8e2ba672dadbd",
+    "url": "/my.portfolio/static/media/2.dd6e1bde.png"
+  },
+  {
+    "revision": "50d4a493335b3a62677643b32f3829a5",
+    "url": "/my.portfolio/static/media/3.50d4a493.png"
+  },
+  {
+    "revision": "f2d32c97d199c0f1c6bc95db451261e6",
+    "url": "/my.portfolio/static/media/about.f2d32c97.png"
+  },
+  {
+    "revision": "f5d0165e825b9b45e1636c9d68301d07",
+    "url": "/my.portfolio/static/media/blog1.f5d0165e.jpg"
+  },
+  {
+    "revision": "e45b4d4dd1c621dd91291502f12028b4",
+    "url": "/my.portfolio/static/media/blog2.e45b4d4d.jpg"
+  },
+  {
+    "revision": "31bf9772c1ef58ea1b33f56c3fbabb9b",
+    "url": "/my.portfolio/static/media/blog3.31bf9772.jpg"
+  },
+  {
+    "revision": "80605992b5036c334e302da920dce162",
+    "url": "/my.portfolio/static/media/blog4.80605992.jpg"
+  },
+  {
+    "revision": "d95dd2bd1796c6908d194c6891766047",
+    "url": "/my.portfolio/static/media/blog5.d95dd2bd.jpg"
+  },
+  {
+    "revision": "987b4c1144fa2d4c82aa8d874b2a0911",
+    "url": "/my.portfolio/static/media/blog6.987b4c11.jpg"
+  },
+  {
+    "revision": "8a116e0acf74c73dcee645dae9687ec8",
+    "url": "/my.portfolio/static/media/github.8a116e0a.png"
+  },
+  {
+    "revision": "fd0d5546fdbdc85c76c4372a0d51f1bc",
+    "url": "/my.portfolio/static/media/linkedin.fd0d5546.png"
+  },
+  {
+    "revision": "617cb333d780d201279b4995a3616a2d",
+    "url": "/my.portfolio/static/media/profile.617cb333.jpg"
+  },
+  {
+    "revision": "7ac2092ac15b25b4c8a7ead4249b96d1",
+    "url": "/my.portfolio/static/media/skills.7ac2092a.png"
+  },
+  {
+    "revision": "76480b21b3d75e07dad04d662c2a66c6",
+    "url": "/my.portfolio/static/media/wall.76480b21.jpg"
+  },
+  {
+    "revision": "0b0fa8a72cf14d3c487bbaf3ad2da445",
+    "url": "/my.portfolio/static/media/yahoo.0b0fa8a7.png"
+  }
+]);
